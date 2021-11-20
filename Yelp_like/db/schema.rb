cf113ab536de8995fde61ad00802151f9d940283
Christine Lo <christine.lo.hayes@gmail.com>
2021-11-19 23:40:24 -0500

    longinfix

--- Yelp_like/db/schema.rb
+++ Yelp_like/db/schema.rb
@@ -19,27 +19,26 @@ ActiveRecord::Schema.define(version: 2021_11_17_192815) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "average_score", default: 0
   end
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.interger "score"
+    t.string "score"
+    t.string "interger"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
+
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
   end
 
-  add_foreign_key "reviews", "airlines"
-  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "restaurants"
 end
