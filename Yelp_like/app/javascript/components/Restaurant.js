9437416d696479386c07c036e7404791b1134023
Christine Lo <christine.lo.hayes@gmail.com>
2021-11-25 12:18:33 -0500

    fix source url for image

--- Yelp_like/app/javascript/components/Restaurant.js
+++ Yelp_like/app/javascript/components/Restaurant.js
@@ -1,6 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Rating from './Rating/Rating';
+
+
+
 export default function Restaurant({ image_url, name, id, location, average_score }) {
+
+  let imageSrc = "https://maps.googleapis.com/maps/api/staticmap?markers=";
+  imageSrc += location.replace(/ /g, "+");
+  imageSrc += ",Toronto,Ontario&zoom=13&scale=1&size=600x300&maptype=roadmap&format=png&visual_refresh=true&key=AIzaSyC62WGE4DlS6wzMTkLrq3EIszj2qUUpa1o";
+  
   return (
     <article className='rest'>
       <div className='img-container'>
@@ -8,12 +17,17 @@ export default function Restaurant({ image_url, name, id, location, average_scor
       </div>
       <div className='rest-footer'>
         <h3>{name}</h3>
-        <h4>{average_score}</h4>
+        <h4> <Rating score={average_score} /></h4>
+       
         <p>{location}</p>
         <Link to={`/restaurant/${id}`} className='btn btn-primary btn-details'>
           details
         </Link>
       </div>
+      
+      <img src={imageSrc}/>
+
     </article>
   )
+  
 }
--- Yelp_like/db/seeds.rb
+++ Yelp_like/db/seeds.rb
@@ -9,57 +9,68 @@ restaurants = Restaurant.create([
   { 
     name: "Aloette Restaurant",
     image_url: "https://aloetterestaurant.com/wp-content/uploads/2017/10/Aloette-Interiors_002.jpg",
-    location: "163 Spadina Avenue, 1st Floor, Toronto"
+    location: "107 Winners Circle",
+    average_score: "4"
   },
   { 
     name: "George",
     image_url: "https://img2.10bestmedia.com/Images/Photos/358410/381787-526062837422989-1836650623-n_55_660x440.jpg",
-    location: "111C Queen Street E, Toronto"
+    location: "1819 Queen St E",
+    average_score: "5"
   }, 
   { 
     name: "Canoe",
     image_url: "https://3di6zv3beqwj3om56f47wlza-wpengine.netdna-ssl.com/wp-content/uploads/2015/06/6canoe.jpg",
-    location: "66 Wellington Street W, 54th Floor, Toronto"
+    location: "66 Wellington Street W, 54th Floor",
+    average_score: "3"
   }, 
   { 
     name: "DailLo",
     image_url: "https://media.cntraveler.com/photos/5b22bfe4f7e9307cb983b46e/16:9/w_2240,c_limit/DaiLo__2018__BAC1382.jpg",
-    location: "1503 College Street, Toronto"
+    location: "1503 College Street",
+    average_score: "4"
   }, 
   { 
     name: "Miku",
     image_url: "https://mikutoronto.com/wp-content/uploads/2015/03/dining-room-2-1030x687.jpg",
-    location: "10 Bay Street, 105, Toronto"
+    location: "10 Bay Street 105",
+    average_score: "3"
   }, 
   { 
     name: "Jacobs & Co. Steakhouse",
     image_url: "https://b.zmtcdn.com/data/pictures/2/8905322/1cf4a57c4e45075ded8fda4ed14cbef3.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*",
-    location: "12 Brant Street, Toronto"
+    location: "12 Brant Street",
+    average_score: "4"
   }, 
   { 
     name: "Fishman Lobster Clubhouse Restaurant",
     image_url: "https://img1.wsimg.com/isteam/ip/35d04655-b77e-4a9a-a5fd-ca709d04eedf/lobster6.jpg/:/rs=w:1300,h:800",
-    location: "4020 Finch Avenue E, Toronto"
+    location: "4020 Finch Avenue E",
+    average_score: "5"
   }, 
   { 
     name: "Sushi on Bloor",
     image_url: "https://media-cdn.tripadvisor.com/media/photo-s/0e/34/1e/50/outside-of-the-restaurant.jpg",
-    location: "525 Bloor Street W, Toronto"
+    location: "525 Bloor Street W",
+    average_score: "4"
   }, 
   { 
     name: "Yasu",
     image_url: "https://images.squarespace-cdn.com/content/v1/533078b0e4b0ccd5fa57e1ff/1461544750843-EA5RAW3HA7Y1JBBHLBCW/YASU+Toronto+Omakase",
-    location: "81 Harbord Street, Toronto"
+    location: "81 Harbord Street",
+    average_score:"3"
   }, 
   { 
     name: "Kinka Izakaya",
     image_url: "https://images.getbento.com/accounts/5d21d7abc160b36d5cd820a4adda5083/media/xU8L3LAR66rorb68DPnQ_20191129-0E0A0156.jpg?w=1200&fit=crop&auto=compress,format&h=600",
-    location: "398 Church Street, Toronto"
+    location: "398 Church Street",
+    average_score: "4"
   }, 
   { 
     name: "Hokkaido Ramen Santouka",
     image_url: "http://1.bp.blogspot.com/-8s5y7exnFPw/UKWeSsJ5fyI/AAAAAAAACVw/NP9_NpOobTQ/s1600/DSC01597.JPG",
-    location: "91 Dundas Street E, Toronto"
+    location: "91 Dundas Street E",
+    average_score: "3"
   }
 ])
 reviews = Review.create([{
